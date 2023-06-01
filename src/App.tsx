0dc1405b2a24eafb7a10f63d1e5cfb96c6deb028
DelayDragon <1130445410@qq.com>
2023-06-02 01:45:25 +0800

    feat：添加基础Button组件的样式；

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
 
-import { useState } from 'react'
+import {Button,ButtonType,ButtonSize} from './components/Button/button'
+
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          测试
-        </a>
-        <a href="https://react.dev" target="_blank">
-          sss
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Vite + React</h2>
-      <h3>Vite + React</h3>
-      <h4>Vite + React</h4>
-      <h5>Vite + React</h5>
-      <h6>Vite + React</h6>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button disabled size={ButtonSize.Small}>default</Button>
+
+      <Button size={ButtonSize.Small}>hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>large HELLO</Button>
+      <Button btnType={ButtonType.Link} size={ButtonSize.Small} href='http://www.baidu.com'>Baidu Link</Button>
+      <Button btnType={ButtonType.Link} disabled href='http://www.baidu.com'>Baidu Link</Button>
+
     </>
   )
 }
