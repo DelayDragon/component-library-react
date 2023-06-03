3de8fa2eabc19113c63f51fa904413266f9e5065
DelayDragon <1130445410@qq.com>
2023-06-03 21:31:00 +0800

    feat：添加Menu组件与子组件MenuItem；添加组件样式；

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 
-import {Button,ButtonType,ButtonSize} from './components/Button/button'
+import { Button, ButtonType, ButtonSize } from './components/Button/button'
+import { Menu } from './components/Menu/menu'
+import { MenuItem } from './components/Menu/menuItem'
 
 function App() {
   return (
@@ -10,7 +12,18 @@ function App() {
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>large HELLO</Button>
       <Button btnType={ButtonType.Link} target='_blank' size={ButtonSize.Small} href='http://www.baidu.com'>Baidu Link</Button>
       <Button btnType={ButtonType.Link} disabled href='http://www.baidu.com'>Baidu Link</Button>
-
+      <br />
+      <Menu defaultIndex={0} mode='vertical' onSelect={(index) => console.log(index)}>
+        <MenuItem index={0}>
+          cool link 1
+        </MenuItem>
+        <MenuItem index={1} disabled>
+          cool link 2
+        </MenuItem>
+        <MenuItem index={2}>
+          cool link 3
+        </MenuItem>
+      </Menu>
     </>
   )
 }
---
+++ src/components/Menu/menu.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import classNames from "classnames";
+
+type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: number) => void
+export interface MenuProps {
+    defaultIndex?: number;
+    className?: string;
+    mode?: MenuMode;
+    style?: React.CSSProperties;
+    children?: React.ReactNode
+    onSelect?: SelectCallback
+}
+
+interface IMenuContext {
+    index: number;
+    onSelect?:SelectCallback
+}
+
+export const MenuContext = React.createContext<IMenuContext>({index: 0})
+
+export const Menu: React.FC<MenuProps> = (props) => {
+    const {
+        className,
+        mode,
+        style,
+        children,
+        defaultIndex,
+        onSelect
+    } = props
+    const [currentActive, setCurrentActive] = useState(defaultIndex)
+
+    const classes = classNames('viking-menu', className, {
+        'menu-vertical': mode === 'vertical'
+    })
+    const handleClick = (index: number) => {
+        setCurrentActive(index)
+        if(onSelect){
+            onSelect(index)
+        }
+    }
+    const passedContext: IMenuContext = {
+        index: currentActive ? currentActive : 0,
+        onSelect: handleClick
+    }
+    return (
+        <ul className={classes} style={style} data-testid='test-menu'>
+            <MenuContext.Provider value={passedContext}>
+            {children}
+            </MenuContext.Provider>
+        </ul>
+    )
+}
+
+Menu.defaultProps = {
+    defaultIndex: 0,
+    mode: 'horizontal'
+}
