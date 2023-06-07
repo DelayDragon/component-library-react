defb27952305f3c0ec1ae05849cc9216604cc896
DelayDragon <1130445410@qq.com>
2023-06-07 13:07:50 +0800

    update：更新Button组件水平居中；

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -1,12 +1,15 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { type Meta, type StoryObj } from '@storybook/react';
+import { Button } from '../../components/Button/Button';
 
-// import { Button } from './Button';
-import { Button } from '../components/Button/Button';
+import { CenterDecorator } from './CenterDecorator';
+
+
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
   title: 'Example/Button',
   component: Button,
+  decorators: [CenterDecorator],
   tags: ['autodocs'],
   argTypes: {
     backgroundColor: { control: 'color' },
---
+++ src/stories/Button/CenterDecorator.tsx
@@ -0,0 +1,6 @@
+import './CenterDecorator.scss'
+
+
+export const CenterDecorator = (Story: any) => {
+    return <div className={'button-global'}><Story/></div>
+}
