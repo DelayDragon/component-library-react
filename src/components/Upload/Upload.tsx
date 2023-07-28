cae4854fd267b44d3554227739aca01c2b52565e
DelayDragon <1130445410@qq.com>
2023-07-29 06:33:03 +0800

    🚀update：更新Upload组件：添加上传列表及其样式；
    上传列表
    1、添加上传列表清除方法；
    2、添加上传文件的状态图标；

--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -1,29 +1,57 @@
-import React, { useRef, ChangeEvent } from "react";
+import React, { useRef, ChangeEvent, useState } from "react";
 import axios from 'axios'
-
+import { UploadList } from "./UploadList";
 import { Button } from "../Button/Button";
+
+export type UploadFileStaus = 'ready' | 'uploading' | 'success' | 'error'
+export interface UploadFile {
+    uid: string;
+    size: number;
+    name: string;
+    status?: UploadFileStaus;
+    percent?: number;
+    raw?: File;
+    response?: any;
+    error?: any;
+}
 
 export interface UploadProps {
     action: string;
+    defaultFileList?: UploadFile[];
     beforeUpload?: (file: File) => boolean | Promise<File>
     onProgress?: (precentage: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
     onChange?: (file: File) => void;
+    onRemove?: (file: UploadFile) => void
 }
 
 export const Upload: React.FC<UploadProps> = (props) => {
     const {
         action,
+        defaultFileList,
         beforeUpload,
         onProgress,
         onSuccess,
         onError,
-        onChange
+        onChange,
+        onRemove
     } = props
 
+    const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || [])
     const fileInput = useRef<HTMLInputElement>(null)
 
+    const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
+        setFileList(prevList => {
+            return prevList.map(file => {
+                if (file.uid === updateFile.uid) {
+                    return { ...file, ...updateObj }
+                } else {
+                    return file
+                }
+            })
+        })
+    }
     const handleClick = () => {
         if (fileInput.current) {
             fileInput.current.click()
@@ -37,6 +65,14 @@ export const Upload: React.FC<UploadProps> = (props) => {
         uploadFiles(files)
         if (fileInput.current) {
             fileInput.current.value = ''
+        }
+    }
+    const handleRemove = (file: UploadFile) => {
+        setFileList((prevList) => {
+            return prevList.filter(item => item.uid !== file.uid)
+        })
+        if(onRemove){
+            onRemove(file)
         }
     }
     const uploadFiles = (files: FileList) => {
@@ -58,6 +94,15 @@ export const Upload: React.FC<UploadProps> = (props) => {
         })
     }
     const post = (file: File) => {
+        let _file: UploadFile = {
+            uid: Date.now() + 'upload-file',
+            status: 'ready',
+            name: file.name,
+            size: file.size,
+            percent: 0,
+            raw: file
+        }
+        setFileList([_file, ...fileList])
         const formData = new FormData()
         formData.append(file.name, file)
         axios.post(action, formData, {
@@ -68,6 +113,8 @@ export const Upload: React.FC<UploadProps> = (props) => {
                 if (e.total) {
                     let percentage = Math.round((e.loaded * 100) / e.total) || 0
                     if (percentage < 100) {
+                        updateFileList(_file, {percent: percentage, status: 'uploading'})
+
                         if (onProgress) {
                             onProgress(percentage, file)
                         }
@@ -75,23 +122,28 @@ export const Upload: React.FC<UploadProps> = (props) => {
                 }
             }
         }).then(res => {
+            updateFileList(_file, {status: 'success', response: res.data})
             console.log(res);
             if (onSuccess) {
                 onSuccess(res.data, file)
             }
-            if(onChange){
+            if (onChange) {
                 onChange(file)
             }
         }).catch(err => {
+            updateFileList(_file, {status: 'error', response: err})
+
             console.log(err);
             if (onError) {
                 onError(err, file)
             }
-            if(onChange){
+            if (onChange) {
                 onChange(file)
             }
         })
     }
+    console.log(fileList);
+
     return (
         <div className="viking-upload-component">
             <Button
@@ -107,6 +159,7 @@ export const Upload: React.FC<UploadProps> = (props) => {
                 type="file"
                 onChange={handleFileChange}
             ></input>
+            <UploadList fileList={fileList} onRemove={handleRemove}></UploadList>
         </div>
     )
 }
--- src/stories/Upload/Upload.stories.tsx
+++ src/stories/Upload/Upload.stories.tsx
@@ -1,27 +1,32 @@
 import { type Meta, type StoryObj } from '@storybook/react'
-import { Upload } from '../../components/Upload/Upload'
+import { Upload, UploadFile } from '../../components/Upload/Upload'
 import { action } from '@storybook/addon-actions'
 
+const defaultFileList: UploadFile[] = [
+  { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percent: 30 },
+  { uid: '122', size: 1234, name: 'xyz.md', status: 'success', percent: 30 },
+  { uid: '121', size: 1234, name: 'eyiha.md', status: 'error', percent: 30 }
+]
 
-const checkFileSize = (file: File) => {
-    if(Math.round(file.size / 1024) > 50){
-        alert('file too big')
-        return false
-    }
-    return true
-}
-const filePromise = (file: File) => {
-    const newFile = new File([file], 'new_name_docx', {type: file.type})
-    return Promise.resolve(newFile)
-}
+// 测试beforUpload的函数个例
+// const checkFileSize = (file: File) => {
+//     if(Math.round(file.size / 1024) > 50){
+//         alert('file too big')
+//         return false
+//     }
+//     return true
+// }
+// const filePromise = (file: File) => {
+//     const newFile = new File([file], 'new_name_docx', {type: file.type})
+//     return Promise.resolve(newFile)
+// }
 
 const SimpleUpload: React.FC = () => {
     return (
         <Upload
             action='https://jsonplaceholder.typicode.com/posts'
-            beforeUpload={filePromise}
             onChange={action('changed')}
-
+            defaultFileList={defaultFileList}
         ></Upload>
     )
 }
